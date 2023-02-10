662e8d5e8ad8a32a5d3cd57c33cbfc0bda83864b
halimariya <125077576+halimariya@users.noreply.github.com>
2023-02-10 23:53:45 +0600

    Add files via upload

---
+++ if...else.php
@@ -0,0 +1,11 @@
+<?php 
+$araf =310;
+$mirha =30;
+
+if ($araf != $mirha){
+    echo "Araf has not same money mirha";
+} elseif ($araf > $mirha){
+    echo"Araf has same more money mirha";
+}elseif ($araf < $mirha){
+    echo"Araf has same less money mirha";
+}
